fddfeea91420c5db5071ef9ee5fd1cdc37c02189
rootranjan <root.ranjan@gmail.com>
2025-06-13 16:52:59 +0530

    minor cleanup

--- src/token.py
+++ src/token.py
@@ -1,17 +1,2 @@
-# This was once used for token testing. DO NOT ENABLE.
-
-# old_token_format = "shopback_stage_xyz"
-# dev_token = "12345-testing"
-# environment = "sandbox"
-# used_by = ["ci-runner", "deploy-bot"]
-# revocation_date = "2023-07-15"
-
-# SECRET_TOKEN = "flag{sb_l1ve_SH0PBACKL3AK123}"  # Deprecated after June '23
-
-# fallback = "DISABLED"
+# token cleaned
 TOKEN_PLACEHOLDER = None
-# load_from_vault = False
-# vault_path = "secrets/token/sk_stage_legacy"
-
-# NOTE: Flag was moved to Vault. This copy is obsolete.
-# Cleanup tracked under SEC-2369 & SEC-2442
